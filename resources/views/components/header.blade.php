1e7f8a911beb277d470dbfcc82ffb7c506870dc9
Mike <castrom89@gmail.com>
2025-01-18 18:31:57 -0600

    add button-link and nav-link components, and add alpinejs

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,35 @@
-<nav>
-    <a href="/">Home</a>
-    <a href="/jobs">Jobs</a>
-    <a href="/jobs/create">Create</a>
-</nav>
+<header class="bg-blue-900 text-white p-4" x-data="{ open: false }">
+  <div class="container mx-auto flex justify-between items-center">
+    <h1 class="text-3xl font-semibold">
+      <a href="{{ url('/') }}">Workopia</a>
+    </h1>
+    <nav class="hidden md:flex items-center space-x-4">
+        <x-nav-link url="/" :active="request()->is('/')">Home</x-nav-link>
+        <x-nav-link url="/jobs" :active="request()->is('jobs')">All Jobs</x-nav-link>
+        <x-nav-link url="/jobs/saved" :active="request()->is('jobs/saved')">Saved Jobs</x-nav-link>
+        <x-nav-link url="/login" :active="request()->is('login')">Login</x-nav-link>
+        <x-nav-link url="/register" :active="request()->is('register')">Register</x-nav-link>
+        <x-nav-link url="/dashboard" :active="request()->is('dashboard')" icon="gauge">Dashboard</x-nav-link>
+        <x-button-link url="/jobs/create" icon="edit" bgClass="bg-red-500">Create Job</x-button-link>
+    </nav>
+    <button id="hamburger" class="text-white md:hidden flex items-center" x-on:click="open = !open">
+      <i class="fa fa-bars text-2xl"></i>
+    </button>
+  </div>
+  <!-- Mobile Menu -->
+  <div
+    id="mobile-menu"
+    class="md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2"
+    x-cloak
+    x-show="open"
+    @click.away="open = false"
+  >
+    <x-nav-link url="/" :active="request()->is('/')" :mobile="true">Home</x-nav-link>
+    <x-nav-link url="/jobs" :active="request()->is('jobs')" :mobile="true">All Jobs</x-nav-link>
+    <x-nav-link url="/jobs/saved" :active="request()->is('jobs/saved')" :mobile="true">Saved Jobs</x-nav-link>
+    <x-nav-link url="/dashboard" :active="request()->is('dashboard')" :mobile="true">Dashboard</x-nav-link>
+    <x-nav-link url="/login" :active="request()->is('login')" :mobile="true">Login</x-nav-link>
+    <x-nav-link url="/register" :active="request()->is('register')" :mobile="true">Register</x-nav-link>
+    <x-button-link url="/jobs/create" icon="edit" bgClass="bg-red-500" :block="true">Create Job</x-button-link>
+  </div>
+</header>
